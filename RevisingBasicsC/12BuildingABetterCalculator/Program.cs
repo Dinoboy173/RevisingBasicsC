cda499cf3b414d84ef70453e85e625d81379d32f
Jordan Wesson <s201031@students.aie.edu.au>
2021-02-24 11:14:30 +1100

    2:17:29

--- RevisingBasicsC/12BuildingABetterCalculator/Program.cs
+++ RevisingBasicsC/12BuildingABetterCalculator/Program.cs
@@ -6,7 +6,35 @@ namespace _12BuildingABetterCalculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation");
+            }
 
             Console.ReadLine();
         }
